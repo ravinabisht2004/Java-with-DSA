5a11bbd102b238cec28a80c34374858337a38fbc
ravinabisht2004 <ravinasinghbisht159677@gmail.com>
2026-09-03 23:01:53 +0530

    Feat - adding if_else statement questions

--- src/First_Program/If_Else.java
+++ src/First_Program/If_Else.java
@@ -9,20 +9,48 @@ public class If_Else {
         // Salary 
         
         Scanner BasicPay = new Scanner(System.in);
-        System.out.print("Please enter your salary => ");
-        int Salary = BasicPay.nextInt();
+        // System.out.print("Please enter your salary => ");
+        // int Salary = BasicPay.nextInt();
 
-        if (Salary > 25000){
-            Salary = Salary + 3000;
+        // if (Salary > 25000){
+        //     Salary = Salary + 3000;
+        // }
+        // else if(Salary >= 15000){
+        //     Salary = Salary + 2000;
+        // }
+        // else {
+        //     Salary = Salary + 1000;
+        // }
+
+        // System.out.print(Salary);
+
+        System.out.print("Please write a number => ");
+        int number = BasicPay.nextInt();
+
+        //Positive or Negative Number
+
+        // if(number == 0){
+        //     System.out.println("Number is 0");
+        // }
+        // else if (number < 0){
+        //     System.out.println("Number is negative");
+        // }
+        // else{
+        //     System.out.println("Number is positive");
+        // }
+
+        //Even or Odd
+
+        if(number%2 == 0){
+            System.out.print(number + " is Even");
         }
-        else if(Salary >= 15000){
-            Salary = Salary + 2000;
-        }
-        else {
-            Salary = Salary + 1000;
+        else{
+            System.out.println(number + " is Odd");
         }
 
-        System.out.print(Salary);
+
+
+
         
 
 
