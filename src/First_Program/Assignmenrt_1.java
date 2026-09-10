f94b57307c9c15fed37819dd8e72ed8335fd0e8a
Ravina Bisht <ravinasinghbisht159677@gmail.com>
2026-09-10 12:06:33 +0530

    feat - adding area of Circle, Triangle, Rectangle ,Isosceles , Parallelogram

---
+++ src/First_Program/Assignmenrt_1.java
@@ -0,0 +1,87 @@
+package First_Program;
+
+import java.util.Scanner;
+
+public class Assignmenrt_1 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        // --> Area Of Circle Java Program
+
+        /*
+         double area = 0;
+
+         double pi = 3.14;
+
+         System.out.print("Please Enter radius => ");
+         int radius = input.nextInt();
+
+         area = pi * (radius * radius);
+         System.out.println("Area => " + area);
+         
+          */
+
+
+
+        //--> Area Of Triangle
+
+       /*  System.out.print("Enter the height => ");
+        double height = input.nextDouble();
+
+        System.out.print("Enter the width => ");
+        double width = input.nextDouble();
+
+        double area = (width * height)/2;
+
+        System.out.println("Area of Triangle => " + area);*/
+
+
+        //--> Area Of Rectangle Program
+
+        /*System.out.print("Enter the length => ");
+        float length = input.nextFloat();
+         
+        System.out.print("Enter the breadth => ");
+        float breadth = input.nextFloat();
+
+        float area = length * breadth;
+
+        System.out.println("Area of Rectangle => " + area);*/
+
+
+        //--> Area Of Isosceles Triangle
+
+        /*System.out.print("Enter the base => ");
+        double base = input.nextDouble();
+
+        System.out.print("Enter the height => ");
+        double height = input.nextDouble();
+
+        double area = (base*height)/2;
+
+        System.out.println("Area Of Isosceles Triangle => " + area ); */
+
+
+
+        //--> Area Of Parallelogram
+
+       /*  System.out.print("Enter the base => ");
+        int base = input.nextInt();
+
+        System.out.print("Enter the height => ");
+        int height = input.nextInt();
+
+        int area = (base*height);
+
+        System.out.println("Area Of Parallelogram => " + area );*/
+
+
+
+
+
+
+
+    }
+}
